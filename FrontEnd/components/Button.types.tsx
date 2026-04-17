0635de7c5f415affad7aeee92ed5df7942ad591f
Sheila <152979164+SheilaVillanueva@users.noreply.github.com>
2026-04-17 18:07:50 -0300

    Delete FrontEnd/components/Button.types.tsx

--- FrontEnd/components/Button.types.tsx
+++
@@ -1,7 +0,0 @@
-export interface ButtonProps {
-    size?: "small" | "medium" | "large";
-    disabled?: boolean;
-    variant: "amarillo" | "naranja" | "blanco" | "gris";
-    label: string;
-    onClick?: () => void;
-}
