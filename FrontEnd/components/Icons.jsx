dc738d7b61aacbc6570a584235c9b983e39cb48a
CFranchini04 <178452412+CFranchini04@users.noreply.github.com>
2026-04-17 17:14:22 -0300

    modificación en iconoCarga e iconoUsuario
    cambié IconoCarga e IconoUsuario para que quede acorde a lo necesario en Login.jsx

--- FrontEnd/components/Icons.jsx
+++ FrontEnd/components/Icons.jsx
@@ -130,11 +130,78 @@ export function IconoFlecha() {
 
 export function IconoCarga() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24">
-      <path fill="currentColor" fillOpacity={0} stroke="currentColor" strokeDasharray={60} strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12c0 -4.97 4.03 -9 9 -9c4.97 0 9 4.03 9 9c0 4.97 -4.03 9 -9 9c-4.97 0 -9 -4.03 -9 -9Z">
-        <animate fill="freeze" attributeName="stroke-dashoffset" dur="0.6s" values="60;0"/>
-        <animate fill="freeze" attributeName="fill-opacity" begin="0.6s" dur="0.4s" to={1}/>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={100}
+      height={100}
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="none"
+        fillOpacity={0}
+        stroke="#ffc107"
+        strokeDasharray={60}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M3 12c0 -4.97 4.03 -9 9 -9c4.97 0 9 4.03 9 9c0 4.97 -4.03 9 -9 9c-4.97 0 -9 -4.03 -9 -9Z"
+      >
+        <animate
+          fill="freeze"
+          attributeName="stroke-dashoffset"
+          dur="1s"
+          values="60;0"
+        />
+        <animate
+          fill="freeze"
+          attributeName="fill-opacity"
+          begin="1s"
+          dur="1s"
+          to={1}
+        />
       </path>
+    </svg>
+  );
+}
+
+export function IconoUsuario({ style }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={40}
+      height={40}
+      viewBox="0 0 24 24"
+      style={style}
+    >
+      <g
+        fill="none"
+        stroke="#ffc107"
+        strokeDasharray={28}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+      >
+        <path d="M4 21v-1c0 -3.31 2.69 -6 6 -6h4c3.31 0 6 2.69 6 6v1">
+          <animate
+            fill="freeze"
+            attributeName="stroke-dashoffset"
+            dur="0.4s"
+            values="28;0"
+          />
+        </path>
+        <path
+          strokeDashoffset={28}
+          d="M12 11c-2.21 0 -4 -1.79 -4 -4c0 -2.21 1.79 -4 4 -4c2.21 0 4 1.79 4 4c0 2.21 -1.79 4 -4 4Z"
+        >
+          <animate
+            fill="freeze"
+            attributeName="stroke-dashoffset"
+            begin="0.4s"
+            dur="0.4s"
+            to={0}
+          />
+        </path>
+      </g>
     </svg>
   );
 }
