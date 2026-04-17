cd6f2caff62696533c26cca1519ff9f8fd76bead
nicole8086 <nicolequinonez@fiuni.edu.py>
2026-04-17 16:37:46 -0300

    Contabilidad.jsx

--- FrontEnd/Contabilidad.jsx
+++ FrontEnd/Contabilidad.jsx
@@ -1,29 +1,50 @@
+/*MAL IMPLEMENTADO PERO SIRVE PARA PROBARR */ 
 import React from "react";
+import "./App.css";
 
-export default function Contabilidad() {
-  const modules = [
-    "Plan de Cuentas",
-    "Libro Diario",
-    "Libro Mayor",
-    "Balance General",
-    "Balance de Sumas y Saldos",
-    "Balance de Resultados"
-  ];
+const modules = [
+  "Plan de Cuentas",
+  "Libro Diario",
+  "Libro Mayor",
+  "Balance General",
+  "Balance de Sumas y Saldos",
+  "Balance de Resultados"
+];
 
+export default function App() {
   return (
-    <div className="main">
-      <div className="header">
-        Periodo Contable Enero 2026 - Diciembre 2026
-      </div>
+    <div className="container">
+      
+      <aside className="sidebar">
+        <div className="logo">Nombre</div>
 
-      <div className="cards">
-        {modules.map((mod, index) => (
-          <div key={index} className="card">
-            <span>{mod}</span>
-            <button className="btn">📝</button>
-          </div>
-        ))}
-      </div>
+        <ul className="menu">
+          <li>Compras</li>
+          <li>Tesorería</li>
+          <li>Ventas</li>
+          <li>RR.HH</li>
+          <li className="active">Contabilidad</li>
+        </ul>
+
+        <div className="user">El Ingeniero 13</div>
+      </aside>
+
+      <main className="main">
+        
+        <div className="header">
+          Periodo Contable Enero 2026 - Diciembre 2026
+        </div>
+
+        <div className="cards">
+          {modules.map((mod, index) => (
+            <div key={index} className="card">
+              <span>{mod}</span>
+              <button className="btn">📝</button>
+            </div>
+          ))}
+        </div>
+
+      </main>
     </div>
   );
 }
