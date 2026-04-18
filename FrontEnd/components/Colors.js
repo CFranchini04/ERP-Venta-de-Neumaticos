4810441b9db00b633daf07794090c2ba14b42cc0
Sheila <152979164+SheilaVillanueva@users.noreply.github.com>
2026-04-18 12:08:35 -0300

    Create Colors.js
    Colores disponibles para utilizar, esto se utiliza en Buttons,jsx y Lista.jsx por el momento

---
+++ FrontEnd/components/Colors.js
@@ -0,0 +1,18 @@
+export const getColor = (variant) => {
+  switch (variant) {
+    case "amarillo":
+      return "#FFCC00";
+    case "naranja":
+      return "#FFAE00";
+    case "blanco":
+      return "#ffffff";
+    case "gris":
+      return "#444444";
+     case "gris-claro":
+      return "#CECECE";
+      case "rojo":
+      return "#E30613";
+    default:
+      return "#1D1D1D";
+  }
+};
