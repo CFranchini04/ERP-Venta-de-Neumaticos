316862e128499f72c10ecb5b7ea02d915adb5edf
DiogoDelvalle <diogomartinflores@gmail.com>
2026-05-08 11:28:23 -0300

    Se cambio el nombre de la carpeta y ruteo correctamente index,js

--- FrontEnd/src/Pantallas/Compras/index.js
+++ FrontEnd/src/Pantallas/Compras/index.js
@@ -1,7 +1,7 @@
 export { default as Compras } from './Compras';
-export { default as Pedidos } from './Pedidos';
-export { default as Cotizaciones } from './Cotizaciones';
-export { default as OrdenesCompra } from './OrdenesCompra';
-export { default as OrdenesPago } from './OrdenesPago';
-//export { default as Facturas } from './Facturas';
-//export { default as Proveedores } from './Proveedores';
+export { default as Pedidos } from './Pedidos/Pedidos';
+export { default as Cotizaciones } from './Cotizaciones/Cotizaciones';
+export { default as OrdenesCompra } from './OrdenesCompra/OrdenesCompra';
+export { default as OrdenesPago } from './OrdenesPago/OrdenesPago';
+export { default as Facturas } from './Facturas/Facturas';
+export { default as Proveedores } from './Proveedores/Proveedores';
