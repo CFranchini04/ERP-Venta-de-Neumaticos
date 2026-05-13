3d2c7c68dc2801bf2d381fa2234c43cdd93a8435
CFranchini04 <178452412+CFranchini04@users.noreply.github.com>
2026-05-13 17:33:27 -0300

    Cambio de los IDs por el path

--- FrontEnd/src/components/modules.js
+++ FrontEnd/src/components/modules.js
@@ -4,29 +4,9 @@
 import { IconoCompras, IconoTesoreria, IconoVentas, IconoRRHH, IconoContabilidad } from './Icons';
 
 export const MODULOS = [
-  {
-    id: 'compras',
-    label: 'Compras',
-    icon: <IconoCompras />,
-  },
-  {
-    id: 'tesoreria',
-    label: 'Tesorería',
-    icon: <IconoTesoreria />,
-  },
-  {
-    id: 'ventas',
-    label: 'Ventas',
-    icon: <IconoVentas />,
-  },
-  {
-    id: 'rrhh',
-    label: 'RR.HH',
-    icon: <IconoRRHH />,
-  },
-  {
-    id: 'contabilidad',
-    label: 'Contabilidad',
-    icon: <IconoContabilidad />,
-  },
+  { id: '/rrhh',        label: 'RRHH',          icon: <IconoRRHH /> },
+  { id: '/compras',     label: 'Compras',       icon: <IconoCompras /> },
+  { id: '/ventas',      label: 'Ventas',        icon: <IconoVentas /> },
+  { id: '/tesoreria',   label: 'Tesorería',     icon: <IconoTesoreria /> },
+  { id: '/contabilidad',label: 'Contabilidad',  icon: <IconoContabilidad /> },
 ];
