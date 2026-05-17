682bd30739bd59c15c793afeda76a804a64231a8
Sheila <152979164+SheilaVillanueva@users.noreply.github.com>
2026-05-16 21:20:13 -0300

    Update Buttons.jsx
    Hay distintos tamanos de Button

--- FrontEnd/src/components/Buttons.jsx
+++ FrontEnd/src/components/Buttons.jsx
@@ -6,27 +6,46 @@ export const Button = ({
   variant = "amarilloclaro",
   label,
   onClick,
-  size = 20,
+  size = "md",
 }) => {
+
+  const sizes = {
+    sm: {
+      padding: "6px 12px",
+      fontSize: 12,
+    },
+    md: {
+      padding: "10px 18px",
+      fontSize: 14,
+    },
+    lg: {
+      padding: "14px 26px",
+      fontSize: 16,
+    }
+  };
 
   return (
     <button
       onClick={onClick}
       disabled={disabled}
       style={{
-        paddingLeft: 20,
-        paddingRight: 20,
+        ...sizes[size],
+
         background: getColor(variant),
         boxShadow: "0px 2px 2px rgba(0, 0, 0, 0.25)",
         borderRadius: 8,
-        border: "3px solid #4 44444",
+        border: "3px solid #444444",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
         cursor: disabled ? "not-allowed" : "pointer",
       }}
     >
-      <span style={{ color: "#1D1D1D", fontSize: size, fontFamily: "Lato" }}>
+      <span style={{
+        color: "#1D1D1D",
+        fontSize: sizes[size].fontSize,
+        fontFamily: "Lato"
+      }}>
         {label}
       </span>
     </button>
