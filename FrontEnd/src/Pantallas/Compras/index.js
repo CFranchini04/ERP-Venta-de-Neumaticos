4355bd152e6570ae476223337d66ca8847ac94dc
nicole8086 <nicolequinonez@fiuni.edu.py>
2026-05-18 23:51:49 -0300

    Update index.js

--- FrontEnd/src/Pantallas/Compras/index.js
+++ FrontEnd/src/Pantallas/Compras/index.js
@@ -1,6 +1,7 @@
 export { default as Compras } from './Compras';
 export { default as Pedidos } from './Pedidos/Pedidos';
 export { default as Cotizaciones } from './Cotizaciones/Cotizaciones';
+export { default as DetalleCotizaciones } from './Cotizaciones/DetalleCotizaciones';
 export { default as OrdenesCompra } from './OrdenesCompra/OrdenesCompra';
 export { default as OrdenesPago } from './OrdenesPago/OrdenesPago';
 export { default as Facturas } from './Facturas/Facturas';
