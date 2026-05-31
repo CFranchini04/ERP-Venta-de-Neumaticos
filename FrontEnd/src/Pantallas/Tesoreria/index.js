73ac35c9462dabe1930da0649cd3ee6b708ced03
CFranchini04 <178452412+CFranchini04@users.noreply.github.com>
2026-05-31 15:55:04 -0300

    Update index.js

--- FrontEnd/src/Pantallas/Tesoreria/index.js
+++ FrontEnd/src/Pantallas/Tesoreria/index.js
@@ -1,4 +1,4 @@
 export { default as Tesoreria } from './Tesoreria';
 export { default as BancosSaldos } from './BancosSaldos';
 export { default as Cuenta } from './Cuenta';
-export {default as Deposito} from './Deposito';
+export { default as Deposito } from './Deposito';
